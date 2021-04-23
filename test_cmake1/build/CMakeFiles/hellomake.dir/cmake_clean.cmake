58ef75bc1ac1e3e8b2278c8fd625d8c9c511f46e
NamWoo <pre3ice@gmail.com>
2021-04-23 17:32:51 +0900

    init

---
+++ test_cmake1/build/CMakeFiles/hellomake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellomake.dir/hellofunc.c.o"
+  "CMakeFiles/hellomake.dir/hellomake.c.o"
+  "hellomake.pdb"
+  "hellomake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hellomake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
